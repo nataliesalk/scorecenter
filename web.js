8f80f47819195254a5d5bacd1a527466b9973725
NatalieSalk <natalie.salk@tufts.edu>
2013-04-16 14:08:21 -0400

    add origin stuff

--- web.js
+++ web.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 app.use(express.logger());
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+ });
+
 app.get('/', function(request, response) {
   response.send('Hello World!');
 });
@@ -10,3 +16,8 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
+
+
+
+
