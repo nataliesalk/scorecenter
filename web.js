62d46cf70a2e11fe4cd57db60ff68da3d072e0fe
NatalieSalk <natalie.salk@tufts.edu>
2013-04-17 23:36:30 -0400

    working on usersearch

--- web.js
+++ web.js
@@ -110,9 +110,24 @@ app.get('/usersearch', function (request, response) {
   response.send('<!DOCTYPE html><html><head><title>User Search</title></head><body><h1>Find a list of scores for a specific user</h1><form name="search" action="displayuser" method="post">Username: <input type="text" name="username"><input type="submit" value="Submit"></form><p><a href="/">Back to all highscores</a></p></body></html>');
 });
 
-
-
-
+app.post('/displayuser', function (req, res) {
+  var user = req.body.username;
+  var content = '';
+  db.collection('highscores', function (error, collection) {
+    collection.find({username:user}).sort({score:1}, function (err, cursor) {
+      cursor.each(function (err, item) {
+        if (item) {
+          content = content + '<tr><td>' + item.game_title + '</td><td>' + item.score + '</td><td>' + item.created_at + '</td></tr>';
+        }
+        else {
+          db.close();
+          res.set('Content-Type', 'text/html');
+          res.send('<!DOCTYPE html><html><h1>Displaying a list of scores for ' + user + '</h1><table border=1px width=400px><tr><td>Game</td><td>Score</td><td>Date Played</td></tr>' + content + '</table><p><a href="/">Back to all highscores</a></p></html>');
+        }
+      });
+    });
+  });
+});
 
 
 
