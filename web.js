0e48be08313504f5bbccae3843d2895b004981a5
NatalieSalk <natalie.salk@tufts.edu>
2013-04-18 16:15:32 -0400

    try new  /submit.json

--- web.js
+++ web.js
@@ -109,9 +109,9 @@ app.post('/usersearch', function (request, response) {
 app.post('/submit.json', function (request, response) {
    	 db.collection('highscores', function (err, collection) {
    	 	  var date = "{ 'created_at' : " + new Date + "}";
-    	  var username = "{ 'username' : " + request.body.username + "}";
-    	  var game_title = "{ 'game_title' : " + request.body.game_title + "}";
-    	  var score = "{ 'score' : " + request.body.score + "}";
+    	  var username = "{ 'username' : " + request.body.username + "},";
+    	  var game_title = "{ 'game_title' : " + request.body.game_title + "},";
+    	  var score = "{ 'score' : " + request.body.score + "},";
     	  string = '{' + username + game_title + score + date + '}';
     	  console.log(string);
     	  collection.insert(string);
